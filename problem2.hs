2e57bcc5fccad07f198cf23145474706dd8d03a3
Michal Wysokinski <michal@Yoga.HomeDomain>
2019-12-09 23:27:21 +0100

    Solve problem2. Add stub for problem3.

--- problem2.hs
+++ problem2.hs
@@ -1,10 +1,17 @@
-fiblist n = reverse $ fiblist' n
-    where
-        fiblist' 1 = [1]
-        fiblist' 2 = [1, 1]
-        fiblist' n = let fib1:fib2:fiblst = fiblist' (n - 1)
-                         fibcur = fib1 + fib2
-                     in fibcur:fib1:fib2:fiblst
+-- Get fibbonacii numbers not exceeding maxfib
+fibs :: Int -> [Int]
+fibs maxfib = fibsa 2 1 maxfib [2, 1]
+
+fibsa :: Int -> Int -> Int -> [Int] -> [Int]
+fibsa n1 n2 maxfib accum =  if curfib > maxfib
+                            then accum
+                            else fibsa curfib n1 maxfib (curfib:accum)
+                        where curfib = n1 + n2
+
+iseven x = (x `mod` 2) == 0
 
 main = do
-        putStrLn $ show $ (fiblist 12)
+        let f4m = fibs 4000000
+        let f4meven = filter iseven f4m        
+        putStrLn $ show $ f4meven
+        putStrLn $ show $ sum f4meven
