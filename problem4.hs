6e16d6e9633a5e4cc49f4c122ec4d567e7212104
Michal Wysokinski <michal@Yoga.HomeDomain>
2019-12-11 00:59:22 +0100

    Improve readability and performance

--- problem4.hs
+++ problem4.hs
@@ -12,17 +12,15 @@ isPalindrome :: Int -> Bool
 isPalindrome n = (show n) == (reverse $ show n)
 
 
-apply _ [] = []
-apply x (y:ys) = [x*y, x, y]:(apply x ys)
+numberTimesVector _ [] = []
+numberTimesVector x (y:ys) = [x*y, x, y]:(numberTimesVector x ys)
                  
 
-
-applyCommutative xmin xmax = if xmin <= xmax
-                             then (apply xmax [xmin..xmax]) ++ (applyCommutative xmin (xmax - 1))
-                             else []
-
+applyCommutative [] = []
+applyCommutative (x:xs) = (numberTimesVector x (x:xs)) ++ (applyCommutative xs)
+                         
 
 main = do    
-    let c1 = applyCommutative smallest_factor biggest_factor
+    let c1 = applyCommutative [smallest_factor..biggest_factor]
     let c2 = filter (isPalindrome . head) c1
     putStrLn $ show $ maximumBy (comparing head) c2
