299cd886806aaeb79057cc383d08a6d01c7aec0b
Michal Wysokinski <michal@Yoga.HomeDomain>
2019-12-11 20:20:09 +0100

    solve problem 5

---
+++ problem5.m
@@ -0,0 +1,5 @@
+# 2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
+# What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
+# without divisors: 9  11   12   13   14   15   16   17   18   19   20
+
+lcm(9, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20)
